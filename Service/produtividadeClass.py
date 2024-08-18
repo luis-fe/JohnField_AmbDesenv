21c53d9d90a417868294eea0f9cd130347d063fb
luis-fe <luis_engp@hotmail.com>
2024-08-18 16:57:46 -0300

    implementacao

--- Service/produtividadeClass.py
+++ Service/produtividadeClass.py
@@ -23,8 +23,31 @@ class Produtividade():
         group by
 	        co.nomecategoria
         """
+
+        sql2 = """
+        	select
+	            co.nomecategoria, cp."nomeOperador" 
+            from 
+	            "Easy".categoriaoperacao co
+            inner join "Easy"."Operacao" o on
+	            o.categoriaoperacao = id_categoria::Varchar
+            inner join "Easy"."ColetasProducao" cp on
+	            cp."nomeOperacao" = o."nomeOperacao"
+            where
+	        cp."Data" >= %s and cp."Data" <= %s 
+            group by
+	            co.nomecategoria, cp."nomeOperador" 
+        """
         conn = ConexaoPostgreMPL.conexaoEngine()
         consulta = pd.read_sql(sql,conn,params=(self.dataInicio, self.dataFinal))
-        consulta['Realizado%'] = consulta['Realizado'] / consulta['MetaDia']
-        consulta['Realizado%'] = consulta['Realizado%'].round()
+        consulta2 = pd.read_sql(sql2,conn,params=(self.dataInicio, self.dataFinal))
+        consulta2['QtdOperadores'] = 1
+        consulta2 = consulta.groupby(['nomecategoria']).agg(
+        quantidadeOP=('QtdOperadores'),quantidadePc=('sum')).reset_index()
+        consulta =pd.merge(consulta, consulta2, on = 'nomecategoria', how='left')
+
+        consulta['Realizado%'] = (consulta['Realizado'] / consulta['MetaDia'])*100
+        consulta['Realizado%'] = consulta['Realizado%'].round(1)
+
+        #Implementar o Numerero de Operadores que realizacao a funcao no dia
         return consulta
