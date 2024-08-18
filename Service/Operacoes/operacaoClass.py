2a1633053241b04c6751265c80788ccae65f46c2
luis-fe <luis_engp@hotmail.com>
2024-08-18 13:37:20 -0300

    left join

--- Service/Operacoes/operacaoClass.py
+++ Service/Operacoes/operacaoClass.py
@@ -62,11 +62,12 @@ class Operacao():
 	            select  c.*, f."nomeFase",c2."nomecategoria" as "nomeCategoria"  ,to2."tempoPadrao" as "TempoPadrao(s)", f."nomeFase", "Maq/Equipamento"  from "Easy"."Operacao" c
         inner join "Easy"."Fase" f on f."codFase" = c."codFase"
         inner join "Easy"."TemposOperacao" to2 on to2."codOperacao" = c."codOperacao" 
-        inner join "Easy"."categoriaoperacao" c2 on c2.id_categoria::varchar = c."categoriaoperacao"  
+        left join "Easy"."categoriaoperacao" c2 on c2.id_categoria::varchar = c."categoriaoperacao"  
         """
         consulta = pd.read_sql(sql,conn)
         consulta['Pcs/Hora'] = (60*60)/consulta['TempoPadrao(s)']
         consulta['Pcs/Hora'] = consulta['Pcs/Hora'].astype(int)
+        consulta = consulta.fillna('-',inplace=True)
 
         return consulta
     
